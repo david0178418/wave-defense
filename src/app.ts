b5caa305775e9e5f7ece9853e131202b0d737a6a
David Granado <davidjgranado@gmail.com>
2025-03-13 23:07:50 -0500

    More factoring out

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
 import ECSpresso from 'ecspresso';
 import { initializeGameBundle } from '@/bundles/initialize-game.bundle';
 import { mapPanningBundle } from '@/bundles/map-panning.bundle';
-import { Container } from 'pixi.js';
-import type { ActiveControlMap } from './types';
 
 // Feels gross. Need to find better way to handle this information
 declare global {
@@ -20,64 +18,9 @@ game
 		mapSize: 2000,
 		panSpeed: 500,
 	})
-	.addResource('worldContainer', new Container())
-	.addResource('activeKeyMap', controlMap())
 	.install(
 		initializeGameBundle(),
 		mapPanningBundle(),
 	)
 	.eventBus
 	.publish('initializeGame', { game });
-
-function controlMap(): ActiveControlMap {
-	const controlMap = {
-		up: false,
-		down: false,
-		left: false,
-		right: false,
-	};
-
-	window.addEventListener('keydown', (event) => {
-		switch(event.key) {
-			case 'w':
-			case 'ArrowUp':
-				controlMap.up = true;
-				break;
-			case 's':
-			case 'ArrowDown':
-				controlMap.down = true;
-				break;
-			case 'a':
-			case 'ArrowLeft':
-				controlMap.left = true;
-				break;
-			case 'd':
-			case 'ArrowRight':
-				controlMap.right = true;
-				break;
-		}
-	});
-
-	window.addEventListener('keyup', (event) => {
-		switch(event.key) {
-			case 'w':
-			case 'ArrowUp':
-				controlMap.up = false;
-				break;
-			case 's':
-			case 'ArrowDown':
-				controlMap.down = false;
-				break;
-			case 'a':
-			case 'ArrowLeft':
-				controlMap.left = false;
-				break;
-			case 'd':
-			case 'ArrowRight':
-				controlMap.right = false;
-				break;
-		}
-	});
-
-	return controlMap;
-}
--- src/bundles/initialize-game.bundle.ts
+++ src/bundles/initialize-game.bundle.ts
@@ -1,6 +1,7 @@
 import { Bundle } from 'ecspresso';
 import { Application, Container, Graphics } from 'pixi.js';
 import { randomInt } from '@/utils';
+import type { ActiveControlMap } from '@/types';
 
 export function initializeGameBundle() {
 	return new Bundle<Components, Events, Resources>()
@@ -9,6 +10,9 @@ export function initializeGameBundle() {
 			initializeGame: {
 				async handler({ game }, entityManager, resourceManager, eventBus) {
 					console.log('Initializing game');
+
+					resourceManager.add('worldContainer', new Container())
+					resourceManager.add('activeKeyMap', controlMap())
 					const pixi = new Application();
 
 					await pixi.init({
@@ -68,4 +72,57 @@ export function initializeGameBundle() {
 			},
 		})
 		.bundle;
-} 
+}
+
+function controlMap(): ActiveControlMap {
+	const controlMap = {
+		up: false,
+		down: false,
+		left: false,
+		right: false,
+	};
+
+	window.addEventListener('keydown', (event) => {
+		switch(event.key) {
+			case 'w':
+			case 'ArrowUp':
+				controlMap.up = true;
+				break;
+			case 's':
+			case 'ArrowDown':
+				controlMap.down = true;
+				break;
+			case 'a':
+			case 'ArrowLeft':
+				controlMap.left = true;
+				break;
+			case 'd':
+			case 'ArrowRight':
+				controlMap.right = true;
+				break;
+		}
+	});
+
+	window.addEventListener('keyup', (event) => {
+		switch(event.key) {
+			case 'w':
+			case 'ArrowUp':
+				controlMap.up = false;
+				break;
+			case 's':
+			case 'ArrowDown':
+				controlMap.down = false;
+				break;
+			case 'a':
+			case 'ArrowLeft':
+				controlMap.left = false;
+				break;
+			case 'd':
+			case 'ArrowRight':
+				controlMap.right = false;
+				break;
+		}
+	});
+
+	return controlMap;
+}
